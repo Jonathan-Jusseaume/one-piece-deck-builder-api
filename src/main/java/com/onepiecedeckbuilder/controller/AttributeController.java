d096a723c7a56703b5b0f59a7f53a5a40b966639
JotaroE21 <jusseaumejonathan5@gmail.com>
2024-07-19 19:56:07 +0200

    Cors configuration in only one place

--- src/main/java/com/onepiecedeckbuilder/controller/AttributeController.java
+++ src/main/java/com/onepiecedeckbuilder/controller/AttributeController.java
@@ -5,7 +5,6 @@ import com.onepiecedeckbuilder.service.AttributeService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/attributes")
 @RequiredArgsConstructor
 public class AttributeController {

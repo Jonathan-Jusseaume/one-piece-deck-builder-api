3c2c4713588de62230c5207833860f1213972dbf
JotaroE21 <jusseaumejonathan5@gmail.com>
2025-01-11 13:52:57 +0100

    Unused code since implementation of ProductMapper

--- src/main/java/com/onepiecedeckbuilder/dto/Product.java
+++ src/main/java/com/onepiecedeckbuilder/dto/Product.java
@@ -1,8 +1,6 @@
 package com.onepiecedeckbuilder.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.onepiecedeckbuilder.entity.ProductDescriptionEntity;
-import com.onepiecedeckbuilder.entity.ProductEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,7 +9,6 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.time.LocalDate;
-import java.time.ZoneOffset;
 
 @Getter
 @Setter
@@ -27,16 +24,4 @@ public class Product {
     private LocalDate releaseDate;
     @Schema(description = "Label or name of the product", example = "[OP09] FOUR EMPERORS")
     private String label;
-
-    public Product(ProductEntity productEntity, String languageCode) {
-        this.id = productEntity.getId();
-        this.releaseDate = LocalDate.ofInstant(productEntity.getReleaseDate(), ZoneOffset.UTC);
-        for (ProductDescriptionEntity description : productEntity.getDescriptions()) {
-            if (description.getLanguageCode().equals(languageCode)) {
-                this.label = description.getName();
-                break;
-            }
-        }
-    }
-
 }
